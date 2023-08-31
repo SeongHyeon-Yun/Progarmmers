297d0447ec8065fc75b74383fd6ed51e7616574d
SeongHyeon-Yun <127644670+SeongHyeon-Yun@users.noreply.github.com>
2023-08-31 19:33:17 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -0,0 +1,11 @@
+def solution(n, lost, reserve):
+    s = set(lost) & set(reserve)
+    l = set(lost) - s
+    r = set(reserve) - s
+    
+    for i in sorted(r):
+        if i - 1 in l:
+            l.remove(i - 1)
+        elif i + 1 in l:
+            l.remove(i + 1)
+    return n - len(l)
