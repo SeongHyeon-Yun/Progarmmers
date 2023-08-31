29883d9367d790832915c1dd92f759e633d20e7f
SeongHyeon-Yun <127644670+SeongHyeon-Yun@users.noreply.github.com>
2023-08-31 21:07:49 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.py
@@ -0,0 +1,18 @@
+def solution(a, b, c):
+    
+    if a == b == c :
+        answer = (a+b+c)*(a**2+b**2+c**2)*(a**3+b**3+c**3)
+        
+    elif a == b != c :
+        answer = (a+b+c)*(a**2+b**2+c**2)
+        
+    elif a != b == c :
+        answer = (a+b+c)*(a**2+b**2+c**2)
+        
+    elif a == c != b :
+        answer = (a+b+c)*(a**2+b**2+c**2)
+        
+    else :
+        answer = (a+b+c)
+        
+    return answer
