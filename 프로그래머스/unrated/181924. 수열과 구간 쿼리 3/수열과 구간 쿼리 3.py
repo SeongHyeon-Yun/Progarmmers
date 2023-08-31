479740470854a380d99aa276a68f07b2ffd9f1be
SeongHyeon-Yun <127644670+SeongHyeon-Yun@users.noreply.github.com>
2023-08-31 21:10:44 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.12 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -0,0 +1,10 @@
+def solution(arr, queries):
+    answer = []
+    
+    for i in range(len(queries)):
+        temp = arr[queries[i][0]]
+        arr[queries[i][0]] = arr[queries[i][1]]
+        arr[queries[i][1]] = temp
+        
+    answer= arr
+    return answer
