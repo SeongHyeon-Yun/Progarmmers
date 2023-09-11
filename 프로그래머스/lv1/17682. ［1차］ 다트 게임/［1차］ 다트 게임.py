abc8574b6c1effc3f031b6f941322140c236ba0b
SeongHyeon-Yun <127644670+SeongHyeon-Yun@users.noreply.github.com>
2023-09-11 20:03:33 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.15 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -0,0 +1,22 @@
+import re
+
+def solution(dartResult):
+    
+    darts = re.findall('([0-9]+)([SDT])([*#]?)',dartResult)
+    answer = [0] * len(darts)
+    
+    for index,(num, square, option) in enumerate(darts):
+        answer[index] = int(num)
+        if square == 'D':
+            answer[index] = int(num) ** 2
+        elif square == 'T':
+            answer[index] = int(num) ** 3
+            
+        if option == '*':
+            answer[index] *= 2
+            if 0 <= index - 1:
+                answer[index - 1] *= 2
+        elif option == '#':
+            answer[index] *= -1
+            
+    return sum(answer)
