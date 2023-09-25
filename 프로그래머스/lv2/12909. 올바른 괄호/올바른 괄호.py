3f784d19f140dbcd1c61fc6f3283e04d5c986270
SeongHyeon-Yun <127644670+SeongHyeon-Yun@users.noreply.github.com>
2023-09-25 16:55:54 +0900

    [level 2] Title: 올바른 괄호, Time: 8.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,12 +1,12 @@
 def solution(s):
-    stack = []  # 스택을 사용하여 괄호 짝을 검사할 리스트
-
-    for i in s:
-        if i == '(':
-            stack.append('(')
-        elif i == ')':
-            if not stack:  # 스택이 비어있으면 올바르지 않은 괄호 문자열
+    picture = []  # 괄호 문자열을 저장할 스택
+    
+    for char in s:
+        if char == '(':
+            picture.append(char)
+        elif char == ')':
+            if not picture:  # 스택이 비어있으면 올바르지 않은 괄호 문자열
                 return False
-            stack.pop()  # 스택에서 열린 괄호를 제거
-
-    return len(stack) == 0  # 스택이 비어 있으면 올바른 괄호 문자열
+            picture.pop()  # 스택에서 열린 괄호를 제거
+    
+    return len(picture) == 0  # 스택이 비어 있으면 올바른 괄호 문자열
