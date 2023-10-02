37e547986d4a16f5dd9b1da9dce3f8abbb511a70
SeongHyeon-Yun <127644670+SeongHyeon-Yun@users.noreply.github.com>
2023-10-02 20:56:43 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -0,0 +1,9 @@
+def solution(n, words):
+    answer = [0, 0]
+
+    for i in range(1, len(words)):
+        if words[i] in words[:i] or words[i][0] != words[i-1][-1]:
+            return [i%n +1, i//n +1]
+    
+    return answer
+
